8be9de1cb0c3bd41302440c26301e03f87eac227
Власкина Карина ТКИ-141 <vlaskina.karina@yandex.ru>
2026-03-31 10:07:53 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 /**
  * @brief Считывает катет треугольника с клавиатуры
  * @param reports - строка информации
- * @return 1 при ошибке ввода; при корректном вводе - считанное значение
+ * @return 1 при ошибке ввода, при корректном вводе - считанное значение
  * @note Так как дано, что треугольник прямоугольный, проверять правило треугольника не требуется, остаётся проверить
- * только полученные значения на положительность и на то, что введены числа, а не буквы/иное
+ * только полученные значения на положительность и на то, что введены числа
  */
 double getSide(const std::string reports = "");
 
@@ -18,8 +18,8 @@ int main() {
     double side1 = getSide("Enter first side of right triangle:");
     double side2 = getSide("Enter last side of right triangle:");
     RightTriangle newTriangle(side1, side2);
-    std::cout<<"Perimeter of circumscribed round is "<<newTriangle.getLenghtOfRound()<<std::endl;
-    std::cout<<"Square of circumscribed round is "<<newTriangle.getSquareOfRound()<<std::endl;
+    std::cout<<"Периметр описанного круга равен "<<newTriangle.getLenghtOfRound()<<std::endl;
+    std::cout<<"Площадь описанного круга равна "<<newTriangle.getSquareOfRound()<<std::endl;
     return 0;
 }
 
@@ -28,7 +28,7 @@ double getSide(const std::string report) {
     double side = 0;
     std::cin >> side;
     if (std::cin.fail()) {
-        std::cout<<"Error. You must insert only numbers."<<std::endl;
+        std::cout<<"Ошибка!"<<std::endl;
         exit(1);
     }
     return side;
